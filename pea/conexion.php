574385ef9e430c3d516e53abf6442c54644098a7
Ismael Milán Márquez <ish.professional@gmail.com>
2015-06-07 14:13:08 +0200

    Update conexion.php

--- pea/conexion.php
+++ pea/conexion.php
@@ -7,7 +7,7 @@ $con=mysqli_connect($host,$user,$pass);
 if (mysqli_connect_errno()){
 	echo "Fallo al conectar MySQL: " . mysqli_connect_error();
 }
-	mysqli_select_db($con,"esmdb");
+	mysqli_select_db($con,"ESMDB");
 /*La función TESTMYSQLI comprueba si la consulta ha tenido éxito o no*/
 function testmysqli($con,$mysqli){
 	if ($mysqli)
